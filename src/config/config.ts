6f586d7bdb459915f6a7d8aa5aaab30acf76aac0
Beny Peña <bpenasan@everis.com>
2022-01-07 00:25:26 +0100

    fix mongodb connection docker-compose

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
 export const db: string =
   process.env["MONGO_URI"] ||
-  "mongodb://beny:wefox@127.0.0.1:27017/test?authSource=admin";
+  "mongodb://beny:wefox@mongodb:27017/test?authSource=admin";
